a8fd8d4d6239075c1d9375bf897daa3d15a03b53
uthmanduro <udurosinlohun@gmail.com>
2023-09-30 18:21:02 +0100

    api with subtitle created

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String, Integer
-from db import Base
-
-
-class Video(Base):
-    __tablename__ = "videos"
-
-    id = Column(Integer, primary_key=True, index=True)
-    url = Column(String, nullable=False)
